86ca3cf8c492dbdb7a64d148cacaf9a4a099bd73
saif <sirsaif99@gmail.com>
2023-02-23 07:47:30 +0200

    new file structure

--- backend/src/apis/questions.py
+++ backend/src/apis/questions.py
@@ -4,8 +4,8 @@ from typing import Optional
 from edgedb import AsyncIOClient
 from fastapi import APIRouter, Body, Depends, Query
 
-from db import get_client
-from utils import format_query_result, str_to_list
+from backend.src.db import get_client
+from backend.src.utils import format_query_result, str_to_list
 
 question_router = APIRouter()
 
