9740a6a3c7a239698f820d14a735cbff6a4b5fb4
mabagoury <wnajsksos@gmail.com>
2023-05-22 00:20:36 +0300

    fix allowing accepting multiple answers on the same question

--- backend/src/queries.py
+++ backend/src/queries.py
@@ -12,11 +12,27 @@ async def accept_answer(
 ) -> PostID | None:
     return await executor.query_single(
         """\
-        update Answer
-        filter .id = <uuid>$answer_id
-        set{
-            is_accepted := true
-        }\
+        with
+          answer := (
+            select Answer
+            filter .id = <uuid>$answer_id
+          ),
+          question := (
+            select answer.<answers[is Question]
+          ),
+          question_answers := (
+            for answer in question.answers
+            union (
+              update answer
+              set {
+                is_accepted := false
+              }
+            )
+          )
+        update answer
+        set {
+          is_accepted := true
+        }
         """,
         answer_id=answer_id,
     )
--- backend/src/queries/answers/accept_answer.edgeql
+++ backend/src/queries/answers/accept_answer.edgeql
@@ -1,5 +1,21 @@
-update Answer
-filter .id = <uuid>$answer_id
-set{
-    is_accepted := true
+with
+  answer := (
+    select Answer
+    filter .id = <uuid>$answer_id
+  ),
+  question := (
+    select answer.<answers[is Question]
+  ),
+  question_answers := (
+    for answer in question.answers
+    union (
+      update answer
+      set {
+        is_accepted := false
+      }
+    )
+  )
+update answer
+set {
+  is_accepted := true
 }
